2bc4b576e81cc70757562734efc70e01d08ea734
Alex <alexander.orlov@loxal.net>
2017-07-25 23:38:06 +0200

    fix deployment scripts

--- release-as-docker-container.sh
+++ release-as-docker-container.sh
@@ -2,10 +2,13 @@
 
 ./gradlew clean build
 
+DOCKER_IMAGE=muctool
 DOCKER_TAG=latest
 
 # docker login -u loxal
-docker build --tag=loxal/muctool:$DOCKER_TAG .
-docker push loxal/muctool:$DOCKER_TAG
-docker rm -f muctool
-docker run -d -p 180:8080 --env MY_ENV=$my_env --env MY_ENV_SINGLE --label jvm_lang=kotlin --label sans-backing_service --name muctool loxal/muctool:$DOCKER_TAG
+docker build --tag=loxal/$DOCKER_IMAGE:$DOCKER_TAG .
+docker push loxal/$DOCKER_IMAGE:$DOCKER_TAG
+docker rm -f $DOCKER_IMAGE
+docker run -d -p 180:8080 --env MY_ENV=$my_env --env MY_ENV_SINGLE --label jvm_lang=kotlin --label sans-backing_service --name $DOCKER_IMAGE loxal/$DOCKER_IMAGE:$DOCKER_TAG
+
+docker rmi $(docker images -f "dangling=true" -q) # cleanup, GC for dangling images
