f6e2accd8e6e986a89830d6feca5f1f8c4dac7da
Alex <alexander.orlov@loxal.net>
2017-07-25 23:56:07 +0200

    fix script

--- release-as-docker-container.sh
+++ release-as-docker-container.sh
@@ -11,4 +11,4 @@ docker push loxal/$DOCKER_IMAGE:$DOCKER_TAG
 docker rm -f $DOCKER_IMAGE
 docker run -d -p 180:8080 --env MY_ENV=$my_env --env MY_ENV_SINGLE --label jvm_lang=kotlin --label sans-backing_service --name $DOCKER_IMAGE loxal/$DOCKER_IMAGE:$DOCKER_TAG
 
-docker rmi $(docker images -f "dangling=true" -q) # cleanup, GC for dangling images
+docker rmi $(docker images -f "dangling=true"  -q) # cleanup, GC for dangling images
