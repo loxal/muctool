a476d6446916bd6a44f66481216b159f79be5792
Alex <alexander.orlov@loxal.net>
2017-07-29 17:26:03 +0200

    enable SSL without exposing certificate & passwords

---
+++ release.ps1
@@ -0,0 +1,22 @@
+Set-StrictMode -Version Latest
+$ErrorActionPreference = "Stop"
+$PSDefaultParameterValues["*:ErrorAction"] = "Stop"
+
+./gradlew singleJar
+
+$docker_image = "muctool"
+$docker_tag = "latest"
+
+# docker login -u loxal
+docker build --tag=loxal/$docker_image:$docker_tag .
+docker push loxal/$docker_image:$docker_tag
+docker rm -f $docker_image
+docker run -d -p 180:8300 --env MY_ENV=$my_env --env MY_ENV_SINGLE --label jvm_lang=kotlin --label sans-backing_service --name $docker_image loxal/$docker_image:$docker_tag
+
+$danglingImages = $(docker images -f 'dangling=true' -q)
+
+if ([string]::IsNullOrEmpty($danglingImages)){
+    'there are no dangling Docker images'
+} else {
+    docker rmi -f $danglingImages # cleanup, GC for dangling images
+}
--- run.ps1
+++ run.ps1
@@ -1,4 +1,6 @@
+Param([string] $suffix_args)
+
 Function Main() {
-    ./gradlew run --continuous --parallel --build-cache --no-rebuild --no-scan
+    ./gradlew run --continuous --parallel --build-cache --no-rebuild --no-scan $suffix_args
 }
 Main
