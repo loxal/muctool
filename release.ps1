1fe8347ac03477b984478b941c1f3eea23c992d3
Alex <alexander.orlov@loxal.net>
2017-07-29 18:36:22 +0200

    do not push image

--- release.ps1
+++ release.ps1
@@ -5,14 +5,14 @@ $docker_tag = "latest"
 
 # docker login -u loxal
 docker build --tag loxal/${docker_image}:${docker_tag} .
-docker push loxal/${docker_image}:${docker_tag}
+#docker push loxal/${docker_image}:${docker_tag} # do not push until credentials have been removed from application.conf
 docker rm -f $docker_image
 docker run -d -p 180:8300 --env MY_ENV=$docker_image --env MY_ENV_SINGLE --label jvm_lang=kotlin --label sans-backing_service --name $docker_image loxal/${docker_image}:${docker_tag}
 
 $danglingImages = $(docker images -f "dangling=true" -q)
 
 if ([string]::IsNullOrEmpty($danglingImages)){
-    "there are no dangling Docker images"
+    "There are no dangling Docker images"
 } else {
     docker rmi -f $danglingImages # cleanup, GC for dangling images
 }
