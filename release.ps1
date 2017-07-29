11cdfdd85a7d95338289a332e34a0f7936870d84
Alex <alexander.orlov@loxal.net>
2017-07-29 19:24:35 +0200

    expose HTTPS port

--- release.ps1
+++ release.ps1
@@ -7,7 +7,7 @@ $docker_tag = "latest"
 docker build --tag loxal/${docker_image}:${docker_tag} .
 #docker push loxal/${docker_image}:${docker_tag} # do not push until credentials have been removed from application.conf
 docker rm -f $docker_image
-docker run -d -p 180:8300 --env MY_ENV=$docker_image --env MY_ENV_SINGLE --label jvm_lang=kotlin --label sans-backing_service --name $docker_image loxal/${docker_image}:${docker_tag}
+docker run -d -p 180:8300 -p 443:8443 --env MY_ENV=$docker_image --env MY_ENV_SINGLE --label jvm_lang=kotlin --label sans-backing_service --name $docker_image loxal/${docker_image}:${docker_tag}
 
 $danglingImages = $(docker images -f "dangling=true" -q)
 
