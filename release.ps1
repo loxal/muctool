a97dcce21b94921d618652eabcf6a69d66b97478
Alex <alexander.orlov@loxal.net>
2017-07-29 17:35:58 +0200

    fix release script

--- release.ps1
+++ release.ps1
@@ -8,15 +8,15 @@ $docker_image = "muctool"
 $docker_tag = "latest"
 
 # docker login -u loxal
-docker build --tag=loxal/$docker_image:$docker_tag .
-docker push loxal/$docker_image:$docker_tag
+docker build --tag loxal/${docker_image}:${docker_tag} .
+docker push loxal/${docker_image}:${docker_tag}
 docker rm -f $docker_image
-docker run -d -p 180:8300 --env MY_ENV=$my_env --env MY_ENV_SINGLE --label jvm_lang=kotlin --label sans-backing_service --name $docker_image loxal/$docker_image:$docker_tag
+docker run -d -p 180:8300 --env MY_ENV=$my_env --env MY_ENV_SINGLE --label jvm_lang=kotlin --label sans-backing_service --name $docker_image loxal/${docker_image}:${docker_tag}
 
-$danglingImages = $(docker images -f 'dangling=true' -q)
+$danglingImages = $(docker images -f "dangling=true" -q)
 
 if ([string]::IsNullOrEmpty($danglingImages)){
-    'there are no dangling Docker images'
+    "there are no dangling Docker images"
 } else {
     docker rmi -f $danglingImages # cleanup, GC for dangling images
 }
