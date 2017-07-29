3f895242d902726d3fe0f20bd15949e4f4582558
Alex <alexander.orlov@loxal.net>
2017-07-29 18:11:47 +0200

    fix release script

--- release.ps1
+++ release.ps1
@@ -1,7 +1,6 @@
-#Param([string] $suffix_args)
+Param([string] $suffix_args)
 
-#./gradlew singleJar $suffix_args
-./gradlew singleJar
+./gradlew singleJar $suffix_args
 
 $docker_image = "muctool"
 $docker_tag = "latest"
