c233bcc0ddff695c08bdbaa129488934b742e8e8
Alex <alexander.orlov@loxal.net>
2017-07-30 05:16:55 +0200

    introduce redirection tests

--- src/test/kotlin/net/loxal/muctool/AppTest.kt
+++ src/test/kotlin/net/loxal/muctool/AppTest.kt
@@ -10,11 +10,29 @@ import org.jetbrains.ktor.http.HttpStatusCode
 import org.jetbrains.ktor.testing.handleRequest
 import org.jetbrains.ktor.testing.withTestApplication
 import org.junit.Test
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+import kotlin.test.assertNull
 import kotlin.test.assertTrue
 
 class AppTest {
     @Test fun testRequest() = withTestApplication(Application::main) {
+        with(handleRequest(HttpMethod.Get, "/dilbert-quote/index.html")) {
+            assertTrue(requestHandled)
+            assertEquals(HttpStatusCode.MovedPermanently, response.status())
+            assertNull(response.content)
+            assertNotNull(response.headers["Location"])
+            assertEquals("http://sky.loxal.net/dilbert-quote/index.html", response.headers["Location"])
+        }
+        with(handleRequest(HttpMethod.Get, "/dilbert-quote/programmer")) {
+            assertTrue(requestHandled)
+            assertEquals(HttpStatusCode.MovedPermanently, response.status())
+            assertNull(response.content)
+            assertNotNull(response.headers["Location"])
+            assertEquals("http://sky.loxal.net/dilbert-quote/programmer", response.headers["Location"])
+        }
         with(handleRequest(HttpMethod.Get, "/test")) {
             assertEquals(HttpStatusCode.OK, response.status())
             assertTrue(response.content!!.startsWith("Netty's serving..."))
@@ -25,5 +43,9 @@ class AppTest {
         with(handleRequest(HttpMethod.Get, "/")) {
             assertTrue(requestHandled)
         }
+    }
+
+    companion object {
+        val LOG: Logger = LoggerFactory.getLogger(AppTest::class.java)
     }
 }
