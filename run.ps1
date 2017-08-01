72227c7593b5f15359d55cbe87a65e8243878d3c
Alex <alexander.orlov@loxal.net>
2017-08-01 08:05:19 +0200

    improve run script reliability

--- run.ps1
+++ run.ps1
@@ -1,7 +1,14 @@
-Param([string] $suffix_args)
+param([string] $suffix_args)
 
-# add "--debug-jvm" to attach debugger
-Function Main() {
-    ./gradlew run --continuous --parallel --build-cache --no-rebuild --no-scan $suffix_args
+try {
+    # add "--debug-jvm" to attach debugger
+    function main() {
+        ./gradlew run --continuous --parallel --build-cache --no-rebuild --no-scan $suffix_args
+    }
+    main
+} finally {
+    $hangingJavaProcessToStop = [regex]::match((jps), "(\d+)\ DevelopmentHost").Groups[1].Value
+#    kill $hangingJavaProcessToStop
+    Stop-Process -Id $hangingJavaProcessToStop
+    Write-Host "Gracefully killed potentially hanging process $hangingJavaProcessToStop"
 }
-Main
