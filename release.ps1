4e71d49cf46c7f85686d2e74655142d6754023d7
Alex <alexander.orlov@loxal.net>
2017-08-17 00:09:18 +0200

    introduce redirect image

--- release.ps1
+++ release.ps1
@@ -12,8 +12,15 @@ docker rm -f $docker_image
 #docker run -d -p 443:1443 --env MY_ENV=$docker_image --env MY_ENV_SINGLE --label teamcity=buildNumber --label sans-backing_service --name $docker_image loxal/${docker_image}:${docker_tag}
 docker run -d -p 443:1443 -v ~/srv/muctool/logs:/home/svc_usr/logs --env BUILD_COUNTER=$env:BUILD_COUNTER --label buildCounter=$env:BUILD_COUNTER --label sans-backing_service --name $docker_image loxal/${docker_image}:${docker_tag}
 
-$danglingImages = $(docker images -f "dangling=true" -q)
+# Redirect container
+$docker_redirect_image = "http-to-https-redirect"
+cd docker-$docker_redirect_image
+docker build --tag loxal/$docker_redirect_image .
+docker push loxal/$docker_redirect_image:1.0.0
+docker rm -f $docker_redirect_image
+docker run -d --name redirect-https -p 80:80 loxal/$docker_redirect_image:1.0.0
 
+$danglingImages = $(docker images -f "dangling=true" -q)
 if ([string]::IsNullOrEmpty($danglingImages)){
     "There are no dangling Docker images"
 } else {
