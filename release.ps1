d1aad0521d26a9ba3b305cd6cc9e9110fc0bc908
Alex <alexander.orlov@loxal.net>
2017-08-16 23:39:26 +0200

    do no expose port 80

--- release.ps1
+++ release.ps1
@@ -10,7 +10,7 @@ docker build --tag loxal/${docker_image}:${docker_tag} .
 #docker push loxal/${docker_image}:${docker_tag} # do not push until credentials have been removed from application.conf
 docker rm -f $docker_image
 #docker run -d -p 443:1443 --env MY_ENV=$docker_image --env MY_ENV_SINGLE --label teamcity=buildNumber --label sans-backing_service --name $docker_image loxal/${docker_image}:${docker_tag}
-docker run -d -p 80:1180 -p 443:1443 -v ~/srv/muctool/logs:/home/svc_usr/logs --env BUILD_COUNTER=$env:BUILD_COUNTER --label buildCounter=$env:BUILD_COUNTER --label sans-backing_service --name $docker_image loxal/${docker_image}:${docker_tag}
+docker run -d -p 443:1443 -v ~/srv/muctool/logs:/home/svc_usr/logs --env BUILD_COUNTER=$env:BUILD_COUNTER --label buildCounter=$env:BUILD_COUNTER --label sans-backing_service --name $docker_image loxal/${docker_image}:${docker_tag}
 
 $danglingImages = $(docker images -f "dangling=true" -q)
 
