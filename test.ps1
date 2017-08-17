d65733bb3581ff79138f4871ccf05c91da5dbdd6
Alex <alexander.orlov@loxal.net>
2017-08-17 15:29:30 +0200

    adjust redirect

--- test.ps1
+++ test.ps1
@@ -1,5 +1,9 @@
 #!/usr/bin/env powershell
 
+Set-StrictMode -Version Latest
+$ErrorActionPreference = "Stop"
+$PSDefaultParameterValues["*:ErrorAction"] = "Stop"
+
 # add "--debug-jvm" to attach debugger
 Write-Host "args: $args"
 ./gradlew test --no-scan --parallel --no-rebuild --continuous --build-cache $args
