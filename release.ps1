e2ca685f4419d00c2811e48cf18298cf86f44ae8
Alex <alexander.orlov@loxal.net>
2017-08-21 10:20:54 +0200

    add environment variables; add Site Search

--- release.ps1
+++ release.ps1
@@ -10,7 +10,7 @@ docker build --tag loxal/${docker_image}:${docker_tag} .
 #docker push loxal/${docker_image}:${docker_tag} # do not push until credentials have been removed from application.conf
 docker rm -f $docker_image
 #docker run -d -p 443:1443 --env MY_ENV=$docker_image --env MY_ENV_SINGLE --label teamcity=buildNumber --label sans-backing_service --name $docker_image loxal/${docker_image}:${docker_tag}
-docker run -d -p 443:1443 -v ~/srv/muctool/logs:/home/svc_usr/logs --env BUILD_COUNTER=$env:BUILD_COUNTER --label buildCounter=$env:BUILD_COUNTER --label sans-backing_service --name $docker_image loxal/${docker_image}:${docker_tag}
+docker run -d -p 443:1443 -v ~/srv/muctool/logs:/home/svc_usr/logs -v ~/srv/muctool/data:/home/svc_usr/data --env SECURITY_USER_PASSWORD=$env:SECURITY_USER_PASSWORD --env BUILD_NUMBER=$env:BUILD_NUMBER --env SCM_HASH=$env:SCM_HASH --label buildCounter=$env:BUILD_COUNTER --label sans-backing_service --name $docker_image loxal/${docker_image}:${docker_tag}
 
 # Redirect container
 $docker_redirect_image = "http-to-https-redirect"
