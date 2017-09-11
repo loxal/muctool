8cab8c79fc695c2d6b9fd543635f0f1c4cc518ef
Alex <alexander.orlov@loxal.net>
2017-09-11 22:07:59 +0200

    run

--- Dockerfile
+++ Dockerfile
@@ -2,11 +2,11 @@ FROM openjdk:8-jre-alpine
 
 MAINTAINER Alexander Orlov <alexander.orlov@loxal.net>
 
-### execute as non-root user
-ENV SVC_USR svc_usr
-RUN adduser -D -g $SVC_USR $SVC_USR
-USER $SVC_USR
-WORKDIR /home/$SVC_USR
+### execute as non-root user # caused Exception in thread "main" java.net.SocketException: Permission denied
+#ENV SVC_USR svc_usr
+#RUN adduser -D -g $SVC_USR $SVC_USR
+#USER $SVC_USR
+#WORKDIR /home/$SVC_USR
 ### /execute as non-root user
 
 ADD build/libs/*.jar .
--- release.ps1
+++ release.ps1
@@ -10,8 +10,8 @@ docker build --tag loxal/${docker_image}:${docker_tag} .
 #docker push loxal/${docker_image}:${docker_tag} # do not push until credentials have been removed from application.conf
 docker rm -f $docker_image
 #docker run -d -p 443:1443 --env MY_ENV=$docker_image --env MY_ENV_SINGLE --label teamcity=buildNumber --label sans-backing_service --name $docker_image loxal/${docker_image}:${docker_tag}
-sudo chown -R 1000:1000 ~/srv/$docker_image
-sudo chmod -R 744 ~/srv/$docker_image
+#sudo chown -R 1000:1000 ~/srv/$docker_image
+#sudo chmod -R 744 ~/srv/$docker_image
 docker run -d -p 443:1443 -v ~/srv/muctool/logs:/home/svc_usr/logs -v ~/srv/muctool/data:/home/svc_usr/data --env SECURITY_USER_PASSWORD=$env:SECURITY_USER_PASSWORD --env BUILD_NUMBER=$env:BUILD_NUMBER --env SCM_HASH=$env:SCM_HASH --label buildCounter=$env:BUILD_COUNTER --label sans-backing_service --name $docker_image loxal/${docker_image}:${docker_tag}
 
 # Redirect container
