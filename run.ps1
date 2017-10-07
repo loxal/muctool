41e0e85288b2f1cb2b2317f29ba272eadb5c25fa
Alex <alexander.orlov@loxal.net>
2017-10-07 19:57:08 +0200

    cleanup

--- run.ps1
+++ run.ps1
@@ -6,7 +6,6 @@ $PSDefaultParameterValues["*:ErrorAction"] = "Stop"
 
 try {
     Write-Host "args: $args"
-    # add "--debug-jvm" to attach debugger
     # TODO remove --no-rebuild as it prevents hot-reloading of classes?
     ./gradlew run --continuous --parallel --build-cache --no-scan --continue $args # --no-rebuild
 } finally {
