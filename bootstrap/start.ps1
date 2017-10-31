6450fb39bd96b884f509e7cb055f885e47142208
Alex <alexander.orlov@loxal.net>
2017-10-31 11:33:04 +0100

    introduce mining & forging startup update geoip2 library

--- bootstrap/start.ps1
+++ bootstrap/start.ps1
@@ -14,3 +14,32 @@ docker start service-kit
 # core
 docker start muctool
 docker start router
+
+# mining & forging
+function runMisc {
+    nohup ~/minion/miner/mine-zcash-cpu.sh &
+    echo "mine-zcash-cpu.sh started"
+}
+runMisc
+
+function runNemServer {
+    cd ~/minion/miner/nem-server
+    nohup ./nix.runNis.sh &
+    echo "runNemServer - nix.runNis.sh started"
+#    sleep 15m
+    nohup ./nix.runNcc.sh &
+    echo "runNemServer - nix.runNcc.sh started"
+    # start mining in browser
+}
+runNemServer
+
+function runHeatLedger {
+    cd ~/minion/miner/heatledger-*
+#    screen -mS heatledger bin/heatledger
+    nohup bin/heatledger &
+    echo "Heat Ledger started"
+
+# Start forging...
+#     curl 'http://localhost:7733/api/v1/mining/start/secret%20phrase?api_key=PASSWORD'
+}
+runHeatLedger
