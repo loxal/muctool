8e1fd4c3b5d0901a0abb28ec504b0e2c4d0e25ca
Alex <alexander.orlov@loxal.net>
2017-11-04 14:31:41 +0100

    introduce script to start docker containers only

---
+++ bootstrap/start-docker-containers-only.ps1
@@ -0,0 +1,16 @@
+#!/usr/bin/env pwsh
+
+Set-StrictMode -Version Latest
+$ErrorActionPreference = "Stop"
+$PSDefaultParameterValues["*:ErrorAction"] = "Stop"
+
+# tooling
+docker start teamcity-server
+
+# misc
+docker start service-kit
+
+# core
+docker start muctool
+docker start router
+
