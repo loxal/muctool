a7569ef7028d9639379951b704172ce4861556bd
Alex <alexander.orlov@loxal.net>
2017-11-05 13:35:32 +0100

    introduce API Health checks

--- ci/api-health-check.ps1
+++ ci/api-health-check.ps1
@@ -4,12 +4,12 @@ Set-StrictMode -Version Latest
 $ErrorActionPreference = "Stop"
 $PSDefaultParameterValues["*:ErrorAction"] = "Stop"
 
-# add "--debug-jvm" to attach debugger
-Write-Host "args: $args"
-./gradlew test `
+./gradlew :service:clean :service:test `
+    --build-cache `
+    --no-rebuild `
     --no-scan `
     --parallel `
-    --no-rebuild `
-    --continuous `
-    --build-cache `
-    $args # --continue
+    --continue `
+    --tests *ApiHealthCheck `
+    --info `
+    $args
---
+++ service/src/test/kotlin/net/loxal/muctool/ApiHealthCheck.kt
@@ -0,0 +1,145 @@
+/*
+ * MUCtool Web Toolkit
+ *
+ * Copyright 2017 Alexander Orlov <alexander.orlov@loxal.net>. All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.loxal.muctool
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import io.ktor.http.HttpStatusCode
+import okhttp3.Headers
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import org.junit.Assert.*
+import org.junit.Test
+import org.slf4j.LoggerFactory
+import java.util.*
+
+class ApiHealthCheck {
+    @Test
+    @Throws(Exception::class)
+    fun redirectFromHttpNakedDomain() {
+        val request = Request.Builder()
+                .url("http://muctool.de")
+                .headers(Headers.of(CORS_TRIGGERING_REQUEST_HEADER))
+                .build()
+        val response = HTTP_CLIENT.newCall(request).execute()
+        assertEquals(HttpStatusCode.MovedPermanently.value.toLong(), response.code().toLong())
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun redirectFromUnencryptedWWW() {
+        val request = Request.Builder()
+                .url("http://www.muctool.de")
+                .headers(Headers.of(CORS_TRIGGERING_REQUEST_HEADER))
+                .build()
+        val response = HTTP_CLIENT.newCall(request).execute()
+        assertEquals(HttpStatusCode.MovedPermanently.value.toLong(), response.code().toLong())
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun redirectFromWWW() {
+        val request = Request.Builder()
+                .url("https://www.muctool.de")
+                .headers(Headers.of(CORS_TRIGGERING_REQUEST_HEADER))
+                .build()
+        val response = HTTP_CLIENT.newCall(request).execute()
+        assertEquals(HttpStatusCode.OK.value.toLong(), response.code().toLong())
+        assertTrue(response.body()!!.string().contains(productFrontpageMarker))
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun redirectFromHttpApiDomain() {
+        val request = Request.Builder()
+                .url("http://api.muctool.de")
+                .headers(Headers.of(CORS_TRIGGERING_REQUEST_HEADER))
+                .build()
+        val response = HTTP_CLIENT.newCall(request).execute()
+        assertEquals(HttpStatusCode.MovedPermanently.value.toLong(), response.code().toLong())
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun productFrontpageContent() {
+        val request = Request.Builder()
+                .url("https://muctool.de")
+                .headers(Headers.of(CORS_TRIGGERING_REQUEST_HEADER))
+                .build()
+        val response = HTTP_CLIENT.newCall(request).execute()
+        assertEquals(HttpStatusCode.OK.value.toLong(), response.code().toLong())
+        assertTrue(response.body()!!.string().contains("<title>GeoIP Whois</title>"))
+    }
+
+    private fun assureCorsHeaders(headers: Headers, byteCount: Int) {
+        assertEquals(byteCount.toLong(), headers.byteCount())
+        assertEquals("https://example.com", headers.get("access-control-allow-origin"))
+        assertEquals("true", headers.get("access-control-allow-credentials"))
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun apiFrontpageContent() {
+        val request = Request.Builder()
+                .url("https://api.muctool.de")
+                .headers(Headers.of(CORS_TRIGGERING_REQUEST_HEADER))
+                .build()
+        val response = HTTP_CLIENT.newCall(request).execute()
+        assertEquals(HttpStatusCode.OK.value.toLong(), response.code().toLong())
+        assertNotNull(response.body())
+        assertTrue(response.body()!!.string().contains(productFrontpageMarker))
+
+        assertEquals(HttpStatusCode.OK.value.toLong(), response.code().toLong())
+        assertNull(response.headers().get("x-frame-options"))
+        assertNull(response.headers().get("X-Frame-Options"))
+        assureCorsHeaders(response.headers(), 287)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun whois() {
+        val request = Request.Builder()
+                .url("https://api.muctool.de/whois?clientId=0-0-0-0-3&queryIP=185.17.205.98")
+                .headers(Headers.of(CORS_TRIGGERING_REQUEST_HEADER))
+                .build()
+        val response = HTTP_CLIENT.newCall(request).execute()
+
+        assertEquals(HttpStatusCode.OK.value.toLong(), response.code().toLong())
+        assertNotNull(response.body())
+
+        assureCorsHeaders(response.headers(), 248)
+    }
+
+    companion object {
+        private val LOG = LoggerFactory.getLogger(ApiHealthCheck::class.java)
+
+        private val productFrontpageMarker = "<title>GeoIP Whois</title>"
+
+        private val MAPPER = ObjectMapper()
+        private val HTTP_CLIENT = OkHttpClient.Builder()
+                .followRedirects(false)
+                .followSslRedirects(false)
+                .build()
+        private val CORS_TRIGGERING_REQUEST_HEADER = object : HashMap<String, String>() {
+            init {
+                put("origin", "https://example.com")
+            }
+        }
+    }
+}
