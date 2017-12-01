7bda61073b5c8fc7444a4598b2d4acd1e4421d21
Alex <alexander.orlov@loxal.net>
2017-12-01 21:22:25 +0100

    port SSL update script to Linux' shell

--- docker-router/update-ssl-certificate.sh
+++ docker-router/update-ssl-certificate.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/env pwsh
+#!/usr/bin/env sh
 
-#https://certbot.eff.org
+# https://certbot.eff.org
 #cf. https://gist.github.com/cecilemuller/a26737699a7e70a7093d4dc115915de8
 #improve SSL Labs Server Test rating: https://www.digitalocean.com/community/tutorials/how-to-secure-nginx-with-let-s-encrypt-on-ubuntu-14-04
 
@@ -9,18 +9,18 @@ sudo add-apt-repository ppa:certbot/certbot -y
 sudo apt update -y
 sudo apt install certbot -y
 
-docker stop router
-sudo certbot --standalone certonly --expand `
-    -d sky.loxal.net `
-    -d muctool.loxal.net `
-    -d www.loxal.net `
-    -d ci.loxal.net `
-    -d api.loxal.net `
-    -d loxal.net `
-    -d muctool.de `
-    -d www.muctool.de `
-    -d whois.muctool.de `
-    -d api.muctool.de `
+#docker stop router
+sudo certbot certonly --webroot -w /etc/letsencrypt --expand  \
+    -d sky.loxal.net \
+    -d muctool.loxal.net \
+    -d www.loxal.net \
+    -d ci.loxal.net \
+    -d api.loxal.net \
+    -d loxal.net \
+    -d muctool.de \
+    -d www.muctool.de \
+    -d whois.muctool.de \
+    -d api.muctool.de \
     -d doc.muctool.de
 
-docker start router
+#docker start router
