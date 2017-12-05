fcee6e820f5a0124f3c2e1d6a8d7d9c4d281bb31
Alex <alexander.orlov@loxal.net>
2017-12-05 20:55:57 +0100

    translate PowerShell scripts to Linux shell scripts

---
+++ bootstrap/start-teamcity-ci.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env sh
+
+$version="2017.2"
+$docker_network="dev"
+
+docker rm -f teamcity-server
+docker run -d -t --name teamcity-server \
+    -p 8111:8111 \
+    -e TEAMCITY_SERVER_MEM_OPTS="-Xmx2g -XX:MaxPermSize=270m -XX:ReservedCodeCacheSize=350m" \
+    -v ~/srv/teamcity_server:/data/teamcity_server/datadir \
+    -v ~/srv/teamcity_server/logs:/opt/teamcity/logs \
+    jetbrains/teamcity-server:$version
+
+~/buildAgent/bin/agent.sh stop
+~/buildAgent/bin/agent.sh start # run agent on host machine
+
+function start-ci-agent {
+    docker rm -f teamcity-agent-$1
+    docker run -d -t --name teamcity-agent-$1 \
+        -e SERVER_URL="https://ci.loxal.net" \
+        -e AGENT_NAME=$1 \
+        -v ~/srv/teamcity-agent-$1:/data/teamcity_agent/conf \
+        jetbrains/teamcity-agent:$version
+
+    # Add PowerShell to TeamCity Docker Agent
+    docker exec teamcity-agent-$1 curl -L https://github.com/PowerShell/PowerShell/releases/download/v6.0.0-rc/powershell_6.0.0-rc-1.ubuntu.16.04_amd64.deb -o /tmp/sitesearch-ci-powershell.deb
+    docker exec teamcity-agent-$1 apt-get update -y
+    docker exec teamcity-agent-$1 dpkg -i /tmp/sitesearch-ci-powershell.deb
+    docker exec teamcity-agent-$1 apt-get install -f -y
+}
+
+start-ci-agent Merkur
+start-ci-agent Venus
--- bootstrap/start.sh
+++ bootstrap/start.sh
@@ -1,8 +1,4 @@
-#!/usr/bin/env pwsh
-
-Set-StrictMode -Version Latest
-$ErrorActionPreference = "Stop"
-$PSDefaultParameterValues["*:ErrorAction"] = "Stop"
+#!/usr/bin/env sh
 
 # tooling
 docker start teamcity-server
