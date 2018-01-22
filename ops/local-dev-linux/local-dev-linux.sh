baecdcd483b40beb17d177aecdc274b9bfc18f56
Alex <alexander.orlov@loxal.net>
2018-01-22 18:29:34 +0100

    expose persistence folder to non-root directories

--- ops/local-dev-linux/local-dev-linux.sh
+++ ops/local-dev-linux/local-dev-linux.sh
@@ -9,21 +9,21 @@ docker.exe run -d -t --name local-dev-linux \
     -p 23389:3389 \
     -p 5901:5901 \
     -p 5900:5900 \
+    -v /c/Users/alex/my/local-dev-linux:/mnt/my \
+    -v /c/Users/alex/my/local-dev-linux:/home/alex \
     -v /c/:/mnt/c \
     -v /c/Users/alex:/mnt/alex \
     --network dev \
     loxal/local-dev-linux:latest
-#    -v /home/alex/srv/local-dev-linux:/mnt \
-#    ubuntu:bionic
-#    alpine:3.7
-#    debian:latest bash
-#    openjdk:9-jre bash
 
 docker.exe exec -i local-dev-linux /etc/init.d/ssh start
 docker.exe exec -i local-dev-linux /etc/init.d/xrdp start
 docker.exe ps
 
 #term_.command.removeKnownHostByIndex(5)
+
+#TODO put into IntelliJ: bash -c 'ssh root@10.0.75.1 -p 1122'
+#TODO make "cfg" folder available 
 
 
 
