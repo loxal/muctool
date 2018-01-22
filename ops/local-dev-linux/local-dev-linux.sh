9dbee32db3a8d7f2d7a340a26d721677ca4b5827
Alex <alexander.orlov@loxal.net>
2018-01-22 01:20:21 +0100

    introduce Ubuntu init script; update Kotlin plugin

--- ops/local-dev-linux/local-dev-linux.sh
+++ ops/local-dev-linux/local-dev-linux.sh
@@ -2,9 +2,9 @@
 
 docker.exe build --tag loxal/local-dev-linux:latest .
 
+docker.exe network create dev
 docker.exe rm -f local-dev-linux
 docker.exe run -d -t --name local-dev-linux \
-    -p 22:22 \
     -p 23389:3389 \
     -p 5901:5901 \
     -p 5900:5900 \
@@ -12,13 +12,14 @@ docker.exe run -d -t --name local-dev-linux \
     -v /c/Users/alex:/mnt/alex \
     --network dev \
     loxal/local-dev-linux:latest
+#    -p 22:22 \
 #    -v /home/alex/srv/local-dev-linux:/mnt \
 #    ubuntu:bionic
 #    alpine:3.7
 #    debian:latest bash
 #    openjdk:9-jre bash
 
-docker.exe exec -i local-dev-linux /etc/init.d/ssh start
+#docker.exe exec -i local-dev-linux /etc/init.d/ssh start
 docker.exe exec -i local-dev-linux /etc/init.d/xrdp start
 docker.exe ps
 
---
+++ ops/local-dev-linux/ubuntu-bash-init.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+#ln -s /mnt/c/Users/alex/.ssh ~/     # needs to be copied as specific permission need to be set
+ln -s /mnt/c/Users/alex/.bash_aliases ~/
+ln -s /mnt/c/Users/alex/.gitconfig ~/
+
+#sudo visudo # manual step
+
+sudo apt update
+sudo apt upgrade
+sudo apt autoremove -y
+sudo apt install openjdk-8-jdk-headless -y
+sudo apt install docker.io -y
