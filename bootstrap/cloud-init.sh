29b3b61d6cbacc54f1f2af0bbf545d1603befd2a
Alex <alexander.orlov@loxal.net>
2018-03-22 18:53:41 +0100

    introduce test for cloud-init

--- bootstrap/cloud-init.sh
+++ bootstrap/cloud-init.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
 
+touch /srv/remove_this_test
 git clone git@github.com:loxal/muctool.git /srv/muctool
-sh /srv/muctool/bootstrap/start.sh
+sh /srv/muctool/bootstrap/start-docker-containers-only.sh
