61065686a4c54151657ba580fc33778a5ef3149d
Alex <alexander.orlov@loxal.net>
2018-04-04 01:54:12 +0200

    adjust SSL update mechanism

--- docker-router/update-ssl-certificate.sh
+++ docker-router/update-ssl-certificate.sh
@@ -4,10 +4,8 @@
 #cf. https://gist.github.com/cecilemuller/a26737699a7e70a7093d4dc115915de8
 #improve SSL Labs Server Test rating: https://www.digitalocean.com/community/tutorials/how-to-secure-nginx-with-let-s-encrypt-on-ubuntu-14-04
 
-sudo apt install software-properties-common -y
-sudo add-apt-repository ppa:certbot/certbot -y
 sudo apt update -y
-sudo apt install certbot -y
+sudo apt-get install -y certbot -t stretch-backports
 
 #Your cert will expire on 2018-05-02. To obtain a new or tweaked
 sudo certbot certonly --webroot -w /etc/letsencrypt --expand \
