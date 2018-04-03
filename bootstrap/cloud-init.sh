bfe84857c6b049864618f559b31a22b938d1af21
Alex <alexander.orlov@loxal.net>
2018-04-03 23:54:36 +0200

    assign service to sky.loxal.net

--- bootstrap/cloud-init.sh
+++ bootstrap/cloud-init.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
+# TODO introduce a command to feed a shell script via curl from GitHub
+
 touch /srv/remove_this_test
 git clone git@github.com:loxal/muctool.git /srv/muctool
 sh /srv/muctool/bootstrap/recover-on-boot.sh
