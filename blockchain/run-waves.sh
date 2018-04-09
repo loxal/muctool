1072bd2ca0f5b1fe544f4fdb23dd61575c54e1f0
Alex <alexander.orlov@loxal.net>
2018-04-10 01:26:19 +0200

    fix Waves startup script

--- blockchain/run-waves.sh
+++ blockchain/run-waves.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-screen -mS waves java -jar /srv/minion/waves/waves-all-*.jar waves-mainnet.conf
+screen -mS waves java -jar /srv/minion/waves/waves-all-*.jar /srv/minion/waves/waves-mainnet.conf
