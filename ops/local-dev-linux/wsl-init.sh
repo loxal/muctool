1b3e28382e0ce038d4b60b53e29a7284f908aab2
Alex <alexander.orlov@loxal.net>
2018-04-14 14:39:54 +0200

    update WSL init script

--- ops/local-dev-linux/wsl-init.sh
+++ ops/local-dev-linux/wsl-init.sh
@@ -1,14 +1,22 @@
 #!/usr/bin/env sh
 
-#ln -s /mnt/c/Users/alex/.ssh ~/     # needs to be copied as specific permission need to be set
+#sh /mnt/c/Users/alex/my/project/loxal/muctool/ops/local-dev-linux/wsl-init.sh
+
+sudo sed -i -e 's/%sudo\tALL=(ALL:ALL) ALL/%sudo\tALL=(ALL:ALL) NOPASSWD:ALL/g' /etc/sudoers
+
+# needs to be copied as specific permission need to be set
+sudo cp -r /mnt/c/Users/alex/.ssh ~/
+sudo chown -R minion:minion ~/.ssh
+sudo chmod -R 600 ~/.ssh
+
 ln -s /mnt/c/Users/alex/.bash_aliases ~/
 ln -s /mnt/c/Users/alex/.gitconfig ~/
+mkdir ~/.gradle
 ln -s /mnt/c/Users/alex/.gradle/gradle.properties ~/.gradle
 
-#sudo visudo # manual step
-
-sudo apt update
-sudo apt upgrade
-sudo apt autoremove -y
-sudo apt install openjdk-8-jdk-headless -y
-sudo apt install docker.io -y
+sudo apt -y update
+sudo apt -y upgrade
+sudo apt -y autoremove
+sudo apt-get -y install sudo whois vim net-tools rsync netcat uuid curl \
+    iputils-ping git unzip screen \
+    unattended-upgrades
