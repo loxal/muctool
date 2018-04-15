f3154c5c9b008730320cb73cd7694306864ddde9
Alex <alexander.orlov@loxal.net>
2018-04-15 21:09:04 +0200

    add URL to curl DTO

--- service/src/main/kotlin/net/loxal/muctool/DTO.kt
+++ service/src/main/kotlin/net/loxal/muctool/DTO.kt
@@ -24,7 +24,7 @@ import java.security.SecureRandom
 import java.time.Instant
 import java.util.*
 
-data class Curl(val statusCode: Int = 0, val code: Int = 0, val body: String = "", val url: String = "")
+data class Curl(val statusCode: Int = 0, val code: Int = 0, val body: String? = "", val url: String = "")
 
 data class Encoding(
         val raw: String = "",
