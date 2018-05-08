75f1f2daed1f9139087f59100b9c813545130cfd
Alex <alexander.orlov@loxal.net>
2018-05-08 19:38:27 +0200

    update nux build script

--- ops/local-dev-linux/local-dev-linux.sh
+++ ops/local-dev-linux/local-dev-linux.sh
@@ -1,26 +1,28 @@
 #!/usr/bin/env sh
 
-docker.exe build --no-cache --pull --tag loxal/local-dev-linux:latest .
+#docker build --no-cache --pull --tag loxal/local-dev-linux:latest .
+docker build --pull --tag loxal/local-dev-linux:latest .
 
-docker.exe network create dev
-docker.exe rm -f local-dev-linux
-docker.exe run -d -t --name local-dev-linux --hostname nux \
-    -p 22:22 \
+docker network create dev
+docker rm -f local-dev-linux
+docker run -d -t --name local-dev-linux --hostname nux \
+    -p 1122:22 \
     -p 23389:3389 \
     -p 5901:5901 \
     -p 5900:5900 \
         --expose=1025-9000/tcp \
         --expose=1025-9000/udp \
     -v /c/Users/alex/my/local-dev-linux:/home/minion \
+    -v /c/Users/alex/my/local-dev-linux:/root \
     -v /c/:/mnt/c \
     -v /c/Users/alex:/mnt/my \
     --network dev \
     loxal/local-dev-linux:latest
 
-docker.exe exec -i local-dev-linux /etc/init.d/ssh start
-#docker.exe exec -i local-dev-linux /etc/init.d/xrdp start
-docker.exe exec -i local-dev-linux ln -s /mnt/my/.gradle/gradle.properties /home/minion/.gradle
-docker.exe ps
+docker exec -i local-dev-linux /etc/init.d/ssh start
+#docker exec -i local-dev-linux /etc/init.d/xrdp start
+docker exec -i local-dev-linux ln -s /mnt/my/.gradle/gradle.properties /home/minion/.gradle
+docker ps
 
 #TODO make "cfg" folder available
 
