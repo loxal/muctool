4e909eb51a1518809b1eca5642561285d16f65d7
Alex <alexander.orlov@loxal.net>
2018-05-19 23:55:58 +0200

    assign floating IPs

--- bootstrap/cloud-init.sh
+++ bootstrap/cloud-init.sh
@@ -2,6 +2,14 @@
 
 # TODO introduce a command to feed a shell script via curl from GitHub
 
-touch /srv/remove_this_test
+# rescue
+#mount -o remount,rw /
+#e2fsck -D /dev/sda1 -y
+
+#ip addr add 78.46.236.49/32 dev eth0
+ip addr add 78.46.236.49 dev eth0
+#ip addr add 2a01:4f8:2c17:2c::1/128 dev eth0
+ip addr add 2a01:4f8:1c17:8039::1 dev eth0
+
 git clone git@github.com:loxal/muctool.git /srv/muctool
 sh /srv/muctool/bootstrap/recover-on-boot.sh
--- bootstrap/recover-on-boot.sh
+++ bootstrap/recover-on-boot.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
 
+sudo ip addr add 78.46.236.49 dev eth0
+sudo ip addr add 2a01:4f8:1c17:8039::1 dev eth0
+
 # ops & tooling
 docker start teamcity-server
 docker start teamcity-agent-merkur
@@ -10,7 +13,7 @@ docker start muctool
 docker start router
 
 # start scripts
-#sh bootstrap/init-env-secret.sh
+#sh bootstrap/init-env-secret.sh # TODO introduce env variables to CI
 
 # start blockchain nodes
 sh bootstrap/start-blockchain.sh
