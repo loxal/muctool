ba9360b27554171d32fe9f7c157389ecb2e90840
Alex <alexander.orlov@loxal.net>
2018-05-23 00:40:11 +0200

    introduce release router script as a separate script

---
+++ release-router.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+docker network create main
+docker_network=main
+docker_redirect_image="router"
+docker_redirect_image_tag="latest"
+cd docker-$docker_redirect_image
+docker build --tag loxal/${docker_redirect_image}:${docker_redirect_image_tag} .
+docker rm -f $docker_redirect_image
+docker run -d --name $docker_redirect_image \
+    -p 80:80 -p 443:443 \
+    -v /etc/letsencrypt:/etc/letsencrypt \
+    -v /srv/mirror:/srv/mirror \
+    --network $docker_network \
+    loxal/${docker_redirect_image}:${docker_redirect_image_tag}
--- release.sh
+++ release.sh
@@ -23,17 +23,7 @@ docker run -d \
     loxal/${docker_image}:${docker_tag}
 cd ..
 
-docker_redirect_image="router"
-docker_redirect_image_tag="latest"
-cd docker-$docker_redirect_image
-docker build --tag loxal/${docker_redirect_image}:${docker_redirect_image_tag} .
-docker rm -f $docker_redirect_image
-docker run -d --name $docker_redirect_image \
-    -p 80:80 -p 443:443 \
-    -v /etc/letsencrypt:/etc/letsencrypt \
-    -v /srv/mirror:/srv/mirror \
-    --network $docker_network \
-    loxal/${docker_redirect_image}:${docker_redirect_image_tag}
+sh release-router.sh
 
 danglingImages=$(docker images -f "dangling=true" -q)
 if [ "$danglingImages" ]; then
