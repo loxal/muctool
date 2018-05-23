228344d194ed4719dcf5f5c26a2e14b2ac95e27f
Alex <alexander.orlov@loxal.net>
2018-05-23 20:20:30 +0200

    cleanup wss example

--- waves/src/main/resources/main.js
+++ waves/src/main/resources/main.js
@@ -17,83 +17,25 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-var socket = null;
+let socket = null;
 
 function connect() {
-    console.log("Begin connect");
-    // socket = new WebSocket("ws://" + window.location.host + "/curl");
-    // socket = new WebSocket("ws://localhost:1180/curl");
-    // socket = new WebSocket("ws://api.muctool.de/ws");
-    // socket = new WebSocket("wss://api.muctool.de/wss/curl");
     socket = new WebSocket("wss://api.muctool.de/curl");
 
     socket.onerror = function () {
-        console.log("socket error");
+        console.warn("socket error");
     };
 
     socket.onopen = function () {
-        write("Connected");
+        console.warn("Connected");
     };
 
-    socket.onclose = function (evt) {
-        var explanation = "";
-        if (evt.reason && evt.reason.length > 0) {
-            explanation = "reason: " + evt.reason;
-        } else {
-            explanation = "without a reason specified";
-        }
-
-        write("Disconnected with close code " + evt.code + " and " + explanation);
-        setTimeout(connect, 5000);
+    socket.onclose = function (e) {
+        console.warn(e);
+        setTimeout(connect, 500);
     };
 
-    socket.onmessage = function (event) {
-        received(event.data.toString());
+    socket.onmessage = function (e) {
+        console.warn(e.data.toString());
     };
 }
-
-function received(message) {
-    write(message);
-}
-
-function write(message) {
-    var line = document.createElement("p");
-    line.className = "message";
-    line.textContent = message;
-
-    var messagesDiv = document.getElementById("messages");
-    messagesDiv.appendChild(line);
-    messagesDiv.scrollTop = line.offsetTop;
-}
-
-function onSend() {
-    var input = document.getElementById("commandInput");
-    if (input) {
-        var text = input.value;
-        if (text && socket) {
-            socket.send(text);
-            // input.value = "";
-        }
-    }
-}
-
-function start() {
-    connect();
-
-    document.getElementById("sendButton").onclick = onSend;
-    document.getElementById("commandInput").onkeydown = function (e) {
-        if (e.keyCode == 13) {
-            onSend();
-        }
-    };
-}
-
-function initLoop() {
-    if (document.getElementById("sendButton")) {
-        start();
-    } else {
-        setTimeout(initLoop, 300);
-    }
-}
-
-initLoop();
