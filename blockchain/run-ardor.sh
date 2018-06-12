eb25cf8fd35870e4b05a39e706e4cb7fbcce46fc
Alex <alexander.orlov@loxal.net>
2018-06-12 21:14:27 +0800

    update script

--- blockchain/run-ardor.sh
+++ blockchain/run-ardor.sh
@@ -3,5 +3,6 @@
 cd /srv/minion/ardor
 screen -dmS ardor ./run.sh
 
+sleep 2
 # start forging
 curl http://localhost:27876/nxt?requestType=startForging --data "secretPhrase=$ARDOR_SECRET_PHRASE_ENCODED"
