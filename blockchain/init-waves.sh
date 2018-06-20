c361732fdfa226b59526b3d1f0b5950db8f9928b
Alex <alexander.orlov@loxal.net>
2018-06-21 00:36:28 +0200

    cleanup

--- blockchain/init-waves.sh
+++ blockchain/init-waves.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env sh
 
-echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823
-sudo apt-get -y update
-sudo apt-get install -y sbt
-
-cd /srv/minion
-git clone git@github.com:wavesplatform/Waves.git
-cd Waves
-
-sbt packageAll
+#echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
+#sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823
+#sudo apt-get -y update
+#sudo apt-get install -y sbt
+#
+#cd /srv/minion
+#git clone git@github.com:wavesplatform/Waves.git
+#cd Waves
+#
+#sbt packageAll
 #sbt -Dnetwork=testnet packageAll
 #sbt test
 
-curl -LO https://github.com/wavesplatform/Waves/releases/download/v0.10.3/waves-all-0.10.3.jar
+curl -LO https://github.com/wavesplatform/Waves/releases/download/v0.13.3/waves-all-0.13.3.jar
--- blockchain/run-waves.sh
+++ blockchain/run-waves.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env sh
 
+#curl -LO https://github.com/wavesplatform/Waves/releases/download/v0.13.3/waves-all-0.13.3.jar
 screen -dmS waves java -jar /srv/minion/waves/waves-all-*.jar /srv/minion/waves/waves-mainnet.conf
