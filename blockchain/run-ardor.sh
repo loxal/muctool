a32ff35f390032182541feee4930d441db483352
Alex <alexander.orlov@loxal.net>
2018-07-08 01:09:08 +0200

    cleanup

--- blockchain/run-ardor.sh
+++ blockchain/run-ardor.sh
@@ -3,6 +3,6 @@
 cd /srv/minion/ardor
 screen -dmS ardor ./run.sh
 
-sleep 2
+sleep 5
 # start forging
-curl http://localhost:27876/nxt?requestType=startForging --data "secretPhrase=$ARDOR_SECRET_PHRASE_ENCODED" # does not work: curl: (7) Failed to connect to localhost port 27876: Connection refused
+curl http://localhost:27876/nxt?requestType=startForging --data "secretPhrase=$ARDOR_SECRET_PHRASE_ENCODED" # does not work?: curl: (7) Failed to connect to localhost port 27876: Connection refused
