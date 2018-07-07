c2b0de0d0c922b711235c1d76c5b257bb2422d7f
Alex <alexander.orlov@loxal.net>
2018-07-07 16:35:07 +0200

    increment docker stacks abstract

--- bootstrap/run-DNS-crypt.sh
+++ bootstrap/run-DNS-crypt.sh
@@ -9,7 +9,7 @@ docker rm -f dnscrypt-server
 docker run -d --name dnscrypt-server \
     -p 1443:443/udp -p 1443:443/tcp \
     -v /srv/dnscrypt-server/zones:/opt/unbound/etc/unbound/zones \
-    jedisct1/dnscrypt-server init -N alf.loxal.net:1443 -E 78.47.232.28:1443
+    jedisct1/dnscrypt-server init -N alf.loxal.net:1443 -E 88.99.37.232:1443
 docker exec dnscrypt-server /opt/unbound/sbin/unbound-checkconf
 docker start dnscrypt-server
 docker logs dnscrypt-server -f
--- ops/run-elk.sh
+++ ops/run-elk.sh
@@ -2,4 +2,4 @@
 
 docker swarm init --advertise-addr 88.99.37.232
 docker swarm join --token SWMTKN-1-blub-blib 88.99.37.232:2377
-docker stack deploy --prune
+docker stack deploy -c elk-stack.yaml --prune elk
