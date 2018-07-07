5ccaa8c12b19bca92aa99daf3a6b449ffaa8c477
Alex <alexander.orlov@loxal.net>
2018-07-08 00:37:06 +0200

    introduce BI service

--- blockchain/run-ardor.sh
+++ blockchain/run-ardor.sh
@@ -5,4 +5,4 @@ screen -dmS ardor ./run.sh
 
 sleep 2
 # start forging
-curl http://localhost:27876/nxt?requestType=startForging --data "secretPhrase=$ARDOR_SECRET_PHRASE_ENCODED"
+curl http://localhost:27876/nxt?requestType=startForging --data "secretPhrase=$ARDOR_SECRET_PHRASE_ENCODED" # does not work: curl: (7) Failed to connect to localhost port 27876: Connection refused
