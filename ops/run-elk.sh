5001b9de170fada1c350d0aa7e2328a0657a1412
Alex <alexander.orlov@loxal.net>
2018-07-08 01:27:01 +0200

    cleanup

--- ops/run-elk.sh
+++ ops/run-elk.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env sh
 
 docker swarm init --advertise-addr 88.99.37.232
-docker swarm join --token SWMTKN-1-blub-blib 88.99.37.232:2377
 docker stack deploy -c elk-stack.yaml --prune elk
