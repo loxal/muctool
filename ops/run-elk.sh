e2f369a79041947fa4c763905195db033916b8e6
Alex <alexander.orlov@loxal.net>
2018-07-10 01:51:31 +0200

    introduce Swarm PoC

--- ops/run-elk.sh
+++ ops/run-elk.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env sh
 
-docker swarm leave --force
-docker swarm init --advertise-addr 88.99.37.232
-sysctl -w vm.max_map_count=262144
+#docker swarm leave --force
+#docker swarm init --advertise-addr 88.99.37.232
+#sysctl -w vm.max_map_count=262144
+
 docker stack rm elk
 docker stack deploy -c elk-stack.yaml --prune elk
+
+docker stack ps elk; docker service ls
+curl http://localhost:9200/_cluster/health?pretty
+curl http://localhost:9200/_cat/health?pretty
+curl http://88.99.37.232:9200
