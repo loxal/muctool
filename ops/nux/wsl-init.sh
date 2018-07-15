16ea83cde81874c9e45e27f1d6b41fc7e33687cc
Alex <alexander.orlov@loxal.net>
2018-07-15 20:42:44 +0200

    increment

--- ops/nux/wsl-init.sh
+++ ops/nux/wsl-init.sh
@@ -1,17 +1,25 @@
 #!/usr/bin/env sh
 
+sudo ln -s /mnt/c/Users/alex /mnt/minion # only for WSL, not for native Linux
+#sudo ln -s /mnt/hgfs/alex /mnt/minion # only for native Linux, not for WSL
+
 #sh /mnt/c/Users/alex/my/project/loxal/muctool/ops/nux/wsl-init.sh
 
 sudo sed -i -e 's/%sudo\tALL=(ALL:ALL) ALL/%sudo\tALL=(ALL:ALL) NOPASSWD:ALL/g' /etc/sudoers
 
+cd
 # needs to be copied as specific permission need to be set
-sudo cp -r /mnt/c/Users/alex/.ssh ~/
-sudo chown -R minion:minion ~/.ssh
+sudo cp -r /mnt/minion/.ssh ~/
+sudo chown -R 1000:1000 ~/.ssh
 sudo chmod -R 700 ~/.ssh
 
-ln -s /mnt/c/Users/alex/.gitconfig ~/
-ln -s /mnt/c/Users/alex/.gradle ~/
-ln -s /mnt/c/Users/alex/.kube ~/
+ln -s /mnt/minion/.gitconfig ~/
+ln -s /mnt/minion/.gradle ~/
+ln -s /mnt/minion/.kube ~/
+ln -s /mnt/minion/my ~/
+
+sudo ln -s /mnt/minion/my/nux
+ln -s /mnt/nux/.bash_aliases ~/
 
 sudo apt -y update
 sudo apt -y upgrade
@@ -19,7 +27,3 @@ sudo apt -y autoremove
 sudo apt-get -y install sudo whois vim net-tools rsync netcat uuid curl \
     iputils-ping git unzip screen \
     unattended-upgrades
-
-cd /opt
-sudo curl -O https://download.java.net/java/GA/jdk10/10/binaries/openjdk-10_linux-x64_bin.tar.gz
-sudo tar xfz openjdk-*
