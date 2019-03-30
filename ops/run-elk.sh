9a842d79681983bc60776de11e20d0b3b0b81cef
Alex <alexander.orlov@loxal.net>
2019-03-30 01:51:13 +0100

    fix ELK startup

--- ops/run-elk.sh
+++ ops/run-elk.sh
@@ -2,7 +2,7 @@
 
 #docker swarm leave --force
 #docker swarm init --advertise-addr 88.99.37.232
-sysctl -w vm.max_map_count=262144
+sudo sysctl -w vm.max_map_count=262144
 
 #docker stack rm elk
 #docker stack deploy -c elk-stack.yaml --prune elk
