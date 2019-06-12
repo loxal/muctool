fac30a562c5dee55a1ccb1e67daa4f84283f0c90
Alex <alexander.orlov@loxal.net>
2019-06-12 16:28:54 +0200

    update sis init

--- ops/start-sis-downstream.sh
+++ ops/start-sis-downstream.sh
@@ -17,7 +17,7 @@ docker run -d --name ops-es \
     -e discovery.type=single-node \
     --network main \
     --restart unless-stopped \
-    docker.elastic.co/elasticsearch/elasticsearch:7.1.1
+    docker.elastic.co/elasticsearch/elasticsearch:7.2.0
 #amazon/opendistro-for-elasticsearch:latest
 sleep 20
 
@@ -51,6 +51,26 @@ docker exec -t ops-es \
         "email": ["user@example.com"]
       }'
 
+#docker exec -t ops-es \
+#    curl -X PUT \
+#      http://localhost:9200/svc-singletons/_doc/crawl-status \
+#      -H 'content-type: application/json' \
+#      -d '{
+#        "a2e8d60b-0696-47ea-bc48-982598ee35bd": [
+#            "2019-06-09T10:43:22.178497Z",
+#            "42"
+#        ]
+#    }'
+
+docker exec -t ops-es \
+    curl -X PUT \
+      http://localhost:9200/site-profile/_doc/site-configuration-18e1cb09-b3ec-40e0-8279-dd005771f172 \
+      -H 'Content-Type: application/json' \
+      -d '{
+        "id": ["18e1cb09-b3ec-40e0-8279-dd005771f172"],
+        "secret": ["6dd875d6-b75c-43ae-a7a8-c181fc0b0da6"],
+        "email": ["user@example.com"]
+      }'
 
 curl -X PUT \
   'http://localhost:8001/sites/a9ede989-9d94-41d1-8571-a008318b01db/profile?siteSecret=fbdc4e70-0141-4127-b95b-f9fd2d5e1b93' \
@@ -75,16 +95,39 @@ curl -X PUT \
     "email": "user@example.com"
 }'
 
-#docker exec -t ops-es \
-#    curl -X PUT \
-#      http://localhost:9200/svc-singletons/_doc/crawl-status \
-#      -H 'content-type: application/json' \
-#      -d '{
-#        "a2e8d60b-0696-47ea-bc48-982598ee35bd": [
-#            "2019-06-09T10:43:22.178497Z",
-#            "42"
-#        ]
-#    }'
+curl -X PUT \
+  'http://localhost:8001/sites/a2e8d60b-0696-47ea-bc48-982598ee35bd/profile?siteSecret=04a0afc6-d89a-45c9-8ba8-41d393d8d2f8' \
+  -H 'content-type: application/json' \
+  -d '{
+    "id": "a2e8d60b-0696-47ea-bc48-982598ee35bd",
+    "secret": "04a0afc6-d89a-45c9-8ba8-41d393d8d2f8",
+    "configs": [
+        {
+            "url": "https://api.sitesearch.cloud",
+            "allowUrlWithQuery": false,
+            "pageBodyCssSelector": "body",
+            "sitemapsOnly": false
+        }
+    ],
+    "email": "user@example.com"
+}'
+
+curl -X PUT \
+  'http://localhost:8001/sites/18e1cb09-b3ec-40e0-8279-dd005771f172/profile?siteSecret=6dd875d6-b75c-43ae-a7a8-c181fc0b0da6' \
+  -H 'content-type: application/json' \
+  -d '{
+    "id": "18e1cb09-b3ec-40e0-8279-dd005771f172",
+    "secret": "6dd875d6-b75c-43ae-a7a8-c181fc0b0da6",
+    "configs": [
+        {
+            "url": "https://example.com/page", // use https://www.intrafind.de
+            "allowUrlWithQuery": false,
+            "pageBodyCssSelector": "body",
+            "sitemapsOnly": false
+        }
+    ],
+    "email": "user@example.com"
+}'
 
 curl -X PUT \
   "http://localhost:8001/sites/crawl/status?serviceSecret=$ADMIN_SITE_SECRET" \
@@ -102,21 +145,4 @@ curl -X PUT \
             "pageCount": -1
         }
     ]
-}'
-
-curl -X PUT \
-  'http://localhost:8001/sites/a2e8d60b-0696-47ea-bc48-982598ee35bd/profile?siteSecret=04a0afc6-d89a-45c9-8ba8-41d393d8d2f8' \
-  -H 'content-type: application/json' \
-  -d '{
-    "id": "a2e8d60b-0696-47ea-bc48-982598ee35bd",
-    "secret": "04a0afc6-d89a-45c9-8ba8-41d393d8d2f8",
-    "configs": [
-        {
-            "url": "https://api.sitesearch.cloud",
-            "allowUrlWithQuery": false,
-            "pageBodyCssSelector": "body",
-            "sitemapsOnly": false
-        }
-    ],
-    "email": "user@example.com"
 }'
