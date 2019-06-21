3cd35c2266a632a1ea1d1d282f2b76911a364941
Alex <alexander.orlov@loxal.net>
2019-06-22 01:20:50 +0200

    adjust TeamCity settings

--- ops/start-teamcity-ci.sh
+++ ops/start-teamcity-ci.sh
@@ -8,7 +8,7 @@ docker network create $network
 
 docker rm -f $service_name
 docker run -d -t --name $service_name \
-    -e TEAMCITY_SERVER_MEM_OPTS="-Xmx3g -XX:MaxPermSize=270m -XX:ReservedCodeCacheSize=350m" \
+    --env TEAMCITY_SERVER_MEM_OPTS="-Xmx3G -XX:MaxPermSize=270m -XX:ReservedCodeCacheSize=550m -XX:MaxPermSize=570m" \
     -v /srv/${service_name}:/data/teamcity_server/datadir \
     --network $network \
     jetbrains/${service_name}:${version}
