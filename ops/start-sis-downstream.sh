ec278e0ad6050ae30fb5462d75f4d1fe3476c5e9
Alex <alexander.orlov@loxal.net>
2019-07-07 13:37:03 +0200

    introduce reference site example

--- ops/start-sis-downstream.sh
+++ ops/start-sis-downstream.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env sh
 
-#docker swarm leave --force
-#docker swarm init --advertise-addr 88.99.37.232
 sudo sysctl -w vm.max_map_count=262144
 
 #docker stack rm elk
@@ -10,7 +8,6 @@ sudo sysctl -w vm.max_map_count=262144
 #docker stack ps elk; docker service ls
 #curl http://localhost:9200/_cluster/health?pretty
 #curl http://localhost:9200/_cat/health?pretty
-#curl http://88.99.37.232:9200
 
 docker rm -f ops-es
 docker run -d --name ops-es \
@@ -33,16 +30,6 @@ docker exec -t ops-es \
         "secret": ["56158b15-0d87-49bf-837d-89085a4ec88d"],
         "email": ["user@example.com"]
       }'
-
-#docker exec -t ops-es \
-#    curl -X PUT \
-#      http://localhost:9200/site-profile/_doc/site-configuration-563714f1-96c0-4500-b366-4fc7e734fa1d \
-#      -H 'Content-Type: application/json' \
-#      -d '{
-#        "id": ["563714f1-96c0-4500-b366-4fc7e734fa1d"],
-#        "secret": ["9fe16a55-6772-4d29-854e-7d7d3895e2ca"],
-#        "email": ["user@example.com"]
-#      }'
 
 docker exec -t ops-es \
     curl -X PUT \
@@ -114,11 +101,23 @@ docker exec router curl -X PUT \
     "email": "user@example.com"
 }'
 
+### Reference Site
+
+docker exec -t ops-es \
+    curl -X PUT \
+      http://localhost:9200/site-profile/_doc/site-configuration-563714f1-96c0-4500-b366-4fc7e734fa1d \
+      -H 'Content-Type: application/json' \
+      -d '{
+        "id": ["563714f1-96c0-4500-b366-4fc7e734fa1d"],
+        "secret": ["56158b15-0d87-49bf-837d-89085a4ec88d"],
+        "email": ["user@example.com"]
+      }'
+
 docker exec router curl -X PUT \
-  'http://finder:8001/sites/b7fde685-33f4-4a79-9ac3-ee3b75b83fa3/profile?siteSecret=56158b15-0d87-49bf-837d-89085a4ec88d' \
+  'http://finder:8001/sites/563714f1-96c0-4500-b366-4fc7e734fa1d/profile?siteSecret=56158b15-0d87-49bf-837d-89085a4ec88d' \
   -H 'content-type: application/json' \
   -d '{
-      "id": "b7fde685-33f4-4a79-9ac3-ee3b75b83fa3",
+      "id": "563714f1-96c0-4500-b366-4fc7e734fa1d",
       "secret": "56158b15-0d87-49bf-837d-89085a4ec88d",
       "configs": [
           {
@@ -136,6 +135,11 @@ docker exec router curl -X PUT \
       ],
       "email": "alexander.orlov@loxal.net"
   }'
+
+docker exec router curl -X POST \
+  'http://finder:8001/sites/563714f1-96c0-4500-b366-4fc7e734fa1d/recrawl?siteSecret=56158b15-0d87-49bf-837d-89085a4ec88d&clearIndex=true&isThrottled=true'
+
+###
 
 docker exec router curl -X PUT \
   "http://finder:8001/sites/crawl/status?serviceSecret=$ADMIN_SITE_SECRET" \
@@ -183,9 +187,6 @@ docker exec router curl -X PUT \
     ],
     "email": "user@example.com"
 }'
-
-#curl -X POST \
-#  'http://finder:8001/sites/18e1cb09-b3ec-40e0-8279-dd005771f172/recrawl?siteSecret=6dd875d6-b75c-43ae-a7a8-c181fc0b0da6&clearIndex=true&isThrottled=true'
 
 docker exec router curl -X PUT \
   'http://finder:8001/sites/18e1cb09-b3ec-40e0-8279-dd005771f172/pages?siteSecret=6dd875d6-b75c-43ae-a7a8-c181fc0b0da6' \
