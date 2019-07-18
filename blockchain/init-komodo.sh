eff8c02a2c52928f5bce70849edb9ed63c732f1d
Alex <alexander.orlov@loxal.net>
2019-07-18 08:00:05 +0200

    update blockchain

--- blockchain/init-komodo.sh
+++ blockchain/init-komodo.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env sh
 
 cd /srv/minion
-git clone https://github.com/jl777/komodo --branch master --single-branch
+git clone https://github.com/jl777/komodo
 cd komodo
 #git checkout beta
 git pull
 ./zcutil/fetch-params.sh
-./zcutil/build.sh -j8
+./zcutil/build.sh -j4
 
 #/srv/minion/komodo/src/komodo-cli getnettotals
