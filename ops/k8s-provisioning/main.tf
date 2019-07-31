68e01d6f7a88f3e5eec22ae917e272c13c5545e2
Alex <alexander.orlov@loxal.net>
2019-08-01 00:00:26 +0200

    k8s cleanup

--- ops/k8s-provisioning/main.tf
+++ ops/k8s-provisioning/main.tf
@@ -24,59 +24,6 @@ resource "hcloud_server_network" "controller" {
   ip         = "10.0.1.23"
 }
 
-//resource "hcloud_volume" "persistence" {
-//  server_id = hcloud_server.minion[0].id
-//  name = "${terraform.workspace}-persistence"
-//  size = 10
-//  automount = true
-//  format = "ext4"
-//  labels = {
-//    password = local.password
-//    latest_tenant = terraform.workspace
-//  }
-//  connection {
-//    password = local.password
-//    host = hcloud_server.minion[0].ipv4_address
-//  }
-//
-//  provisioner "remote-exec" {
-//    inline = [
-//      //      "sleep 1 && mkdir /mnt/persistence",
-//      "mkdir /mnt/persistence",
-//      "umount /dev/disk/by-id/scsi-0HC_Volume_${hcloud_volume.persistence.id}",
-//      "echo -n \"${local.password}\" | cryptsetup luksFormat /dev/disk/by-id/scsi-0HC_Volume_${hcloud_volume.persistence.id}",
-//      "echo -n \"${local.password}\" | cryptsetup luksOpen /dev/disk/by-id/scsi-0HC_Volume_${hcloud_volume.persistence.id} encrypted-storage",
-//      "mkfs.ext4 /dev/mapper/encrypted-storage",
-//      "mount -o discard,defaults /dev/mapper/encrypted-storage /mnt/persistence",
-//      "mkdir /mnt/persistence/${terraform.workspace}",
-//      "chown 1000 /mnt/persistence/${terraform.workspace}",
-//    ]
-//  }
-//}
-
-//resource "null_resource" "attach-persistence" {
-//  depends_on = [
-//    hcloud_server.minion,
-//    hcloud_volume.persistence
-//  ]
-//  connection {
-//    password = local.password
-//    host = hcloud_server.minion[0].ipv4_address
-//  }
-//  triggers = {
-//    backup = local.backup
-//  }
-//  provisioner "remote-exec" {
-//    inline = [
-//      //      "sleep 1 && mkdir /mnt/persistence",
-//      "mkdir /mnt/persistence",
-//      "if [ ! -e /dev/mapper/encrypted-storage ]; then echo -n ${local.password} | cryptsetup luksOpen /dev/disk/by-id/scsi-0HC_Volume_${hcloud_volume.persistence.id} encrypted-storage; fi",
-//      "if [ -e /dev/mapper/encrypted-storage ]; then echo `mount -o discard,defaults /dev/mapper/encrypted-storage /mnt/persistence`; else echo `mount -o discard,defaults /dev/disk/by-id/scsi-0HC_Volume_${hcloud_volume.persistence.id} /mnt/persistence`; fi",
-//      "cp -a /mnt/persistence/${terraform.workspace} ${local.backup}",
-//    ]
-//  }
-//}
-
 resource "null_resource" "update-migration" {
   depends_on = [
     hcloud_server.minion
@@ -178,8 +125,9 @@ resource "hcloud_server" "minion" {
     connection {
       host = self.ipv4_address
       //      private_key = file("~/.ssh/id_rsa")
-      password = self.ssh_keys[0]
-      //      password = local.password
+      //      password = self.ssh_keys[0]
+      //            password = local.password
+      //            password = "abcd"
     }
 
     inline = [
@@ -190,7 +138,7 @@ resource "hcloud_server" "minion" {
       "add-apt-repository \"deb [arch=amd64] https://packages.cloud.google.com/apt kubernetes-xenial main\"",
       "add-apt-repository \"deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable\"",
       "apt-get update && apt-get install rsync docker-ce kubeadm sshpass cryptsetup busybox -y",
-      "sysctl -w vm.max_map_count=262144 # required for Elasticsearch",
+      //      "sysctl -w vm.max_map_count=262144 # required for Elasticsearch",
       "sed -i -e 's/%sudo	ALL=(ALL:ALL) ALL/%sudo	ALL=(ALL:ALL) NOPASSWD:ALL/g' /etc/sudoers",
       "iptables -A INPUT -p tcp --match multiport -s 0/0 -d ${hcloud_server.controller[0].ipv4_address} --dports 22,80,179,443,2080,2379,4789,5473,6443,8080,9200,9602,9603,6040:55923 -m state --state NEW,ESTABLISHED -j ACCEPT",
       "iptables -A OUTPUT -p tcp -s ${hcloud_server.controller[0].ipv4_address} -d 0/0 --match multiport --sports 22,80,179,443,2080,2379,4789,5473,6443,8080,9200,9602,9603,6040:55923 -m state --state ESTABLISHED -j ACCEPT",
@@ -229,7 +177,8 @@ resource "hcloud_server" "controller" {
     connection {
       host = self.ipv4_address
       //      private_key = file("~/.ssh/id_rsa")
-      password = local.password
+      //      password = local.password
+      //      password = self.ssh_keys[0]
     }
 
     inline = [
@@ -239,7 +188,7 @@ resource "hcloud_server" "controller" {
       "add-apt-repository \"deb [arch=amd64] https://packages.cloud.google.com/apt kubernetes-xenial main\"",
       "add-apt-repository \"deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable\"",
       "apt-get update && apt-get install rsync docker-ce kubeadm -y",
-      "sysctl -w vm.max_map_count=262144 # required for Elasticsearch",
+      //      "sysctl -w vm.max_map_count=262144 # required for Elasticsearch",
       "sed -i -e 's/%sudo	ALL=(ALL:ALL) ALL/%sudo	ALL=(ALL:ALL) NOPASSWD:ALL/g' /etc/sudoers",
       "adduser --disabled-password --gecos '' minion && usermod -aG sudo minion && usermod --unlock minion",
       "echo 'minion:${local.password}' | chpasswd",
@@ -251,9 +200,10 @@ resource "hcloud_server" "controller" {
       "kubeadm token create --print-join-command > /srv/kubeadm_join",
       "kubectl apply -f /srv/asset/init.yaml",
       "kubectl apply -f https://raw.githubusercontent.com/hetznercloud/csi-driver/master/deploy/kubernetes/hcloud-csi.yml",
-      "sleep 2 && kubectl apply -f /srv/asset/stack.yaml",
+      //      "sleep 2 && kubectl apply -f /srv/asset/stack.yaml",
       //      "kubectl apply -f /srv/exec/init-helm-rbac-config.yaml",
       "curl -L https://git.io/get_helm.sh | bash && helm init",
+      "kubectl apply -f /srv/asset/stack.yaml",
       "iptables -A INPUT -p tcp --match multiport -s 0/0 -d ${hcloud_server.controller[0].ipv4_address} --dports 22,80,179,443,2080,2379,4789,5473,6443,8080,9200,9602,9603,6040:55923 -m state --state NEW,ESTABLISHED -j ACCEPT",
       "iptables -A OUTPUT -p tcp -s ${hcloud_server.controller[0].ipv4_address} -d 0/0 --match multiport --sports 22,80,179,443,2080,2379,4789,5473,6443,8080,9200,9602,9603,6040:55923 -m state --state ESTABLISHED -j ACCEPT",
       "kubectl get svc,node,pvc,deployment,pods,pvc,pv,namespace,serviceaccount,clusterrolebinding -A",
