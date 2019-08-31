07a6fafc90a346ea73bc0593fa7e6485b76561e3
Alex <alexander.orlov@loxal.net>
2019-09-01 00:04:44 +0200

    update TeamCity

--- ops/start-teamcity-ci.sh
+++ ops/start-teamcity-ci.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 # Should be executed on the host with `~/buildAgent` directory
 
-version=2019.1.2-linux
+version=2019.1.3-linux
 service_name=teamcity-server
 network=main
 docker network create $network
