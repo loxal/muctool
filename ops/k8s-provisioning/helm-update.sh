fad45daeb9e056c91345b5e1abf4637ed6391102
Alex <alexander.orlov@loxal.net>
2019-09-03 22:51:42 +0200

    update Ktor

--- ops/k8s-provisioning/helm-update.sh
+++ ops/k8s-provisioning/helm-update.sh
@@ -7,16 +7,16 @@ helmName=muctool
 scp -q -o StrictHostKeyChecking=no root@$k8s_master_node:/etc/letsencrypt/live/loxal.net/cert.pem asset/$helmName
 scp -q -o StrictHostKeyChecking=no root@$k8s_master_node:/etc/letsencrypt/live/loxal.net/privkey.pem asset/$helmName
 
-ssh -q -o StrictHostKeyChecking=no root@$k8s_master_node rm -rf /srv/$helmName
-scp -q -o StrictHostKeyChecking=no -r asset/$helmName root@$k8s_master_node:/srv/$helmName
+ssh -q -o StrictHostKeyChecking=no root@$k8s_master_node rm -rf /opt/$helmName
+scp -q -o StrictHostKeyChecking=no -r asset/$helmName root@$k8s_master_node:/opt/$helmName
 
 ssh -q -o StrictHostKeyChecking=no root@$k8s_master_node helm delete $helmName --purge
 ssh -q -o StrictHostKeyChecking=no root@$k8s_master_node helm delete ingress --purge
 sleep 13
 
-#  helm upgrade $helmName /srv/$helmName --namespace $workspace \
+#  helm upgrade $helmName /opt/$helmName --namespace $workspace \
 ssh -q -o StrictHostKeyChecking=no root@$k8s_master_node \
-  helm install /srv/$helmName --name $helmName --namespace $workspace \
+  helm install /opt/$helmName --name $helmName --namespace $workspace \
   --set app.tenant=$workspace,app.HETZNER_API_TOKEN=$TF_VAR_hetzner_cloud_muctool \
   --set app.dockerRegistrySecret=$TF_VAR_docker_registry_k8s_secret, \
   --set app.meta.scmHash=$SCM_HASH,app.meta.buildNumber=$BUILD_NUMBER, \
@@ -26,12 +26,13 @@ ssh -q -o StrictHostKeyChecking=no root@$k8s_master_node \
 #  helm install --name ingress stable/nginx-ingress \
 #  --set rbac.create=true,controller.hostNetwork=true,controller.kind=DaemonSet
 #ssh -q -o StrictHostKeyChecking=no root@$k8s_master_node \
-#  helm upgrade ingress stable/nginx-ingress --set rbac.create=true,controller.hostNetwork=true,controller.kind=DaemonSet
+#  helm upgrade ingress stable/nginx-ingress \
+#  --set rbac.create=true,controller.hostNetwork=true,controller.kind=DaemonSet
 
 #ssh -q -o StrictHostKeyChecking=no root@$k8s_master_node helm test $helmName --cleanup
 #ssh -q -o StrictHostKeyChecking=no root@$k8s_master_node helm list --all
 
 if [ "$(whoami)" = "alex" ]
 then
-  ssh -q -o StrictHostKeyChecking=no root@$k8s_master_node
+  ssh -q -o StrictHostKeyChecking=no minion@$k8s_master_node
 fi
--- service/build.gradle.kts
+++ service/build.gradle.kts
@@ -70,7 +70,7 @@ tasks.withType<Jar> {
 
 dependencies {
     val kotlinVersion = "1.3.50"
-    val ktorVersion = "1.2.3"
+    val ktorVersion = "1.2.4"
 
     compile("com.maxmind.geoip2:geoip2:2.12.0")
     compile("ch.qos.logback:logback-classic:1.2.3")
