fb3c1cf5733f4d817f7c0bdfafc40052633baa63
Alex <alexander.orlov@loxal.net>
2019-09-20 13:59:20 +0200

    adjust DEV connect

--- ops/k8s-provisioning/dev-connect.sh
+++ ops/k8s-provisioning/dev-connect.sh
@@ -11,3 +11,4 @@ ssh -o StrictHostKeyChecking=no root@$k8s_master_node pkill kubectl
 screen -dmS elasticsearch ssh root@$k8s_master_node kubectl port-forward service/elasticsearch 9200:9200 -n $tenant
 
 ssh -fNL 9201:localhost:9200 root@$k8s_master_node
+ssh -o StrictHostKeyChecking=no root@$k8s_master_node ls -lash
