16153ab2fa12c4e8ca339f6595eeca42c1afe750
Alex <alexander.orlov@loxal.net>
2019-09-23 03:50:51 +0200

    adjust tests

--- service/src/test/kotlin/net/loxal/muctool/AppTest.kt
+++ service/src/test/kotlin/net/loxal/muctool/AppTest.kt
@@ -66,8 +66,8 @@ class AppTest {
             assertNotNull(response.content)
 
             val curl = mapper.readValue(response.byteContent, Curl::class.java)
-            assertEquals(HttpStatusCode.MovedPermanently.value, curl.statusCode)
-            assert(curl.body!!.contains("<title>301 Moved Permanently</title>"))
+            assertEquals(HttpStatusCode.PermanentRedirect.value, curl.statusCode)
+            assert(curl.body!!.contains("<title>308 Permanent Redirect</title>"))
         }
     }
 
