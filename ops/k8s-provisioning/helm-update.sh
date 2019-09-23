4bed18248cf55fbb2099d0dcbb1b651b4dabb1ae
Alex <alexander.orlov@loxal.net>
2019-09-23 10:34:21 +0200

    simplify HELM updates

--- ops/k8s-provisioning/helm-update.sh
+++ ops/k8s-provisioning/helm-update.sh
@@ -11,9 +11,9 @@ scp -q -o StrictHostKeyChecking=no root@$k8s_master_node:/etc/letsencrypt/live/l
 ssh -q -o StrictHostKeyChecking=no root@$k8s_master_node rm -rf /opt/$helmName
 scp -q -o StrictHostKeyChecking=no -r asset/$helmName root@$k8s_master_node:/opt/$helmName
 
-ssh -q -o StrictHostKeyChecking=no root@$k8s_master_node helm delete $helmName --purge
-ssh -q -o StrictHostKeyChecking=no root@$k8s_master_node helm delete ingress --purge
-sleep 13
+#ssh -q -o StrictHostKeyChecking=no root@$k8s_master_node helm delete $helmName --purge
+#ssh -q -o StrictHostKeyChecking=no root@$k8s_master_node helm delete ingress --purge
+#sleep 13
 
 ssh -q -o StrictHostKeyChecking=no root@$k8s_master_node \
   helm upgrade $helmName /opt/$helmName --install --namespace $workspace --recreate-pods \
@@ -26,7 +26,7 @@ ssh -q -o StrictHostKeyChecking=no root@$k8s_master_node \
   --set-string app.volumeHandle=0
 
 ssh -q -o StrictHostKeyChecking=no root@$k8s_master_node \
-  helm upgrade ingress stable/nginx-ingress --install --namespace $workspace \
+  helm upgrade ingress stable/nginx-ingress --install --namespace $workspace --recreate-pods \
   --set rbac.create=true,controller.hostNetwork=true,controller.kind=DaemonSet
 
 #ssh -q -o StrictHostKeyChecking=no root@$k8s_master_node helm test $helmName --cleanup
