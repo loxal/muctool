4395d268e16a99fe28dd55a3ac6d673e5a0dc4f5
Alex <alexander.orlov@loxal.net>
2019-09-23 03:33:10 +0200

    update Waves

--- blockchain/run-waves.sh
+++ blockchain/run-waves.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
-curl -LO https://github.com/wavesplatform/Waves/releases/download/v1.0.2/waves-all-1.0.2.jar
+curl -LO https://github.com/wavesplatform/Waves/releases/download/v1.1.2/waves-all-1.1.2.jar
 screen -dmS waves java -jar /srv/minion/waves/waves-all-*.jar /srv/minion/waves/waves-mainnet.conf
