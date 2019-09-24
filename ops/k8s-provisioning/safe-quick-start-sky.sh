c6ccc4ade27b1b521692ee5e3bc0e064cf48bff8
Alex <alexander.orlov@loxal.net>
2019-09-25 00:20:50 +0200

    introduce safe init script

--- ops/k8s-provisioning/safe-quick-start-sky.sh
+++ ops/k8s-provisioning/safe-quick-start-sky.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env sh
 
+sh ../../blockchain/run-komodo.sh
+sh ../../blockchain/run-waves.sh
+sh ../../blockchain/run-zcash.sh
+
 kubectl apply -f asset/teamcity-ci.yaml --namespace kube-system
 ./helm-update.sh
