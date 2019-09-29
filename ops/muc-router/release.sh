713f42ee4a46a9bfbb0c548f41beace93ce2815c
Alex <alexander.orlov@loxal.net>
2019-09-29 14:53:36 +0200

    re-shift persistence folders

--- ops/muc-router/release.sh
+++ ops/muc-router/release.sh
@@ -4,7 +4,7 @@ docker build --pull \
     --build-arg HASHED_DEFAULT_PASSWORD=$HASHED_DEFAULT_PASSWORD \
     --tag loxal/muc-router:latest .
 
-# docker login
+echo $DOCKER_PASSWORD | docker login --username loxal --password-stdin
 docker push loxal/muc-router:latest
 docker tag loxal/muc-router:latest muctool/muc-router:latest
 docker push muctool/muc-router:latest
