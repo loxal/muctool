96dc8de7621e39c5b62ac46b423953dc4b6a4791
Alex <alexander.orlov@loxal.net>
2019-10-03 11:11:10 +0200

    adjust HELM

--- release.sh
+++ release.sh
@@ -2,5 +2,8 @@
 
 ./gradlew clean build shadowJar --info --no-build-cache
 
-#cd service
-#docker build --pull --tag loxal/muctool:latest .
+cd service
+docker build --pull --tag loxal/muctool:latest .
+
+echo $DOCKER_PASSWORD | docker login --username loxal --password-stdin
+docker push loxal/muctool:latest
