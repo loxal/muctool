7267d82f4260bde274b547892ec61411005b768c
Alex <alexander.orlov@loxal.net>
2019-10-06 00:45:20 +0200

    adjust routes

--- ops/api-health-check.sh
+++ ops/api-health-check.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 
 ./gradlew :service:clean :service:test \
-    --no-scan \
-    --parallel \
-    --continue \
-    --build-cache \
-    --tests ApiHealthCheck \
-    --info \
-    $1
+  --no-scan \
+  --parallel \
+  --continue \
+  --build-cache \
+  --tests ApiHealthCheck \
+  --info \
+  $1
