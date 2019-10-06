825ede0214071d9c69c65b4804f1535736176414
Alex <alexander.orlov@loxal.net>
2019-10-06 23:00:00 +0200

    evolve

--- ops/muc-router/release.sh
+++ ops/muc-router/release.sh
@@ -7,6 +7,9 @@ docker build \
 echo $DOCKER_PASSWORD | docker login --username loxal --password-stdin
 docker push muctool/muc-router:latest
 
+docker tag muctool/muc-router:latest loxal/muc-router:latest
+docker push loxal/muc-router:latest
+
 echo $ADMIN_SITE_SECRET | docker login --username minion --password-stdin docker.muctool.de
 docker tag muctool/muc-router:latest docker.muctool.de/muctool/muc-router:latest
 docker push docker.muctool.de/muctool/muc-router:latest
