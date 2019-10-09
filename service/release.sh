f432de63b1b37dff6c2460d68c4cf7186e162f43
Alex <alexander.orlov@loxal.net>
2019-10-09 15:05:41 +0200

    add alternative push location

--- service/release.sh
+++ service/release.sh
@@ -5,6 +5,6 @@ docker build --tag docker.muctool.de/muctool/muctool:latest .
 echo $ADMIN_SITE_SECRET | docker login --username minion --password-stdin docker.muctool.de
 docker push docker.muctool.de/muctool/muctool:latest
 
-#echo $DOCKER_PASSWORD | docker login --username loxal --password-stdin
-#docker tag docker.muctool.de/muctool/muc-router:latest muctool/muc-router:latest
-#docker push muctool/muc-router:latest
+echo $DOCKER_PASSWORD | docker login --username loxal --password-stdin
+docker tag docker.muctool.de/muctool/muctool:latest muctool/muctool:latest
+docker push muctool/muctool:latest
